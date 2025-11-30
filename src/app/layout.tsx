bc8651ae3db674e987d53acd64fe20a05bd6fbb6
Zeative Media <zaadevofc@gmail.com>
2025-11-30 10:54:50 +0700

    feat: Initialize Next.js project with basic app structure, shadcn/ui setup, and 3D rendering dependencies.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Figtree } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const geistSans = Figtree({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Figtree({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
